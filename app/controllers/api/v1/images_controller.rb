429729af84633d8c36a4b61eaca9d94dd9d25c69
Amy <amytang77@gmail.com>
2021-01-04 15:44:07 -0500

    adding user authorization and handle_unauthorized | set show image and create image actions

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,14 +1,34 @@
 class Api::V1::ImagesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_image, only: [:show, :edit, :update, :destroy]
 
   def index
-    @images = Image.all
+    @images = current_user.images.all
   end
 
   def show
+    if authorized?
+      respond_to do |format|
+        format.json { render :show }
+      end
+    else
+      handle_unauthorized
+    end
   end
 
   def create
+    @image = current_user.images.build(image_params)
+    if authorized?
+      respond_to do |format|
+        if @image.save
+          format.json { render :show, status: :created, location: api_v1_image_path(@image) }
+        else
+          format.json { render json: @image.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      handle_unauthorized
+    end
   end
 
   def update
@@ -20,5 +40,21 @@ class Api::V1::ImagesController < ApplicationController
   private
   def set_image
     @image = Image.find(params[:id])
+  end
+
+  def authorized?
+    @image.user == current_user
+  end
+
+  def handle_unauthorized
+    unless authorized?
+      respond_to do |format|
+        format.json { render :unauthorized, status: 401 }
+      end
+    end
+  end
+
+  def image_params
+    params.require(:image).permit(:title, :description, :picture)
   end
 end
